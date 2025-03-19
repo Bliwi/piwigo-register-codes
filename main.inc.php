fd69df90af661a6085e966777cae329341da68a0
bliwi <bliwi@bliwi.uk>
2025-03-19 16:47:18 -0300

    only increase usage if register is successful

--- main.inc.php
+++ main.inc.php
@@ -18,6 +18,7 @@ define('REGISTER_CODES_PATH', PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/'
 // Hook on to an event to show the administration page.
 add_event_handler('get_admin_plugin_menu_links', 'register_codes_admin_menu');
 add_event_handler('loc_begin_register', 'register_codes_register_init');
+//add_event_handler('login_success', 'register_codes_register_user');
 add_event_handler('init', 'register_codes_init');
 
 function register_codes_admin_menu($menu) {
@@ -45,5 +46,32 @@ function register_codes_init() {
 
 function register_codes_register_init() {
     include(REGISTER_CODES_PATH . '/register_codes_register.php');
+}
+
+function register_codes_register_user() {
+  global $user;
+  $user_register_code = $_SESSION['user_register_code'];
+  $user_id = $user['id'];
+  $username = $user['username'];
+  error_log("User register code: " . $user_register_code . " User ID: " . intval($user_id) . " User Name: " . htmlspecialchars($username), 3, "./_data/logs/custom_debug.log");
+}
+
+// Register the event handler
+add_event_handler('register_user', 'log_user_registration');
+
+/**
+ * Logs the user's name after registration.
+ *
+ * @param array $user_data Array containing user information.
+ */
+function log_user_registration($user_data)
+{
+    global $prefixeTable;
+    $username = $user_data['username'];
+    $user_id = $user_data['id'];
+    $register_code = $_SESSION['user_register_code'];
+    $update_used = 'update ' . $prefixeTable . "register_codes set used = used + 1 where code='$register_code'";
+    pwg_query($update_used);
+    error_log("\nUser registered:" . date('Y-m-d H:i:s') . $username . "User register code: " . $register_code . " User ID: " . $user_id, 3, "./_data/logs/custom_debug.log");
 }
 ?>
--- register_codes_register.php
+++ register_codes_register.php
@@ -1,27 +1,31 @@
 <?php
-if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+if (!defined('PHPWG_ROOT_PATH'))
+  die('Hacking attempt!');
 
 global $template, $prefixeTable;
 
-$template->set_filename('register_codes_register', realpath(REGISTER_CODES_PATH.'register_codes_register.tpl'));
+$template->set_filename('register_codes_register', realpath(REGISTER_CODES_PATH . 'register_codes_register.tpl'));
 $template->assign('REGISTERCODES', array('parsed_content' => $template->parse('register_codes_register', true)), true);
 
 add_event_handler('loc_end_page_header', 'add_register_codes');
 add_event_handler('register_user_check', 'check_code');
 
-function add_register_codes() {
+function add_register_codes()
+{
   global $template;
   $template->set_prefilter('register', 'prefilter_register_codes');
 }
 
-function prefilter_register_codes($content) {
-  $search ="{'Send my connection settings by email'|@translate}"; // bootstrap darkroom, may change if your theme is different
-  return str_replace($search, $search.'{$REGISTERCODES.parsed_content}', $content);
+function prefilter_register_codes($content)
+{
+  $search = "{'Send my connection settings by email'|@translate}"; // bootstrap darkroom, may change if your theme is different
+  return str_replace($search, $search . '{$REGISTERCODES.parsed_content}', $content);
 }
 
-function check_code($errors) {
+function check_code($errors)
+{
   global $prefixeTable, $_POST;
-  if(isset($_POST['register_code'])) {
+  if (isset($_POST['register_code'])) {
     $register_code = $_POST['register_code'];
     $now = date("Y-m-d H:i:s");
     $check_query = 'select * from ' . $prefixeTable . "register_codes where code='$register_code' and (expiry>='$now' or expiry IS NULL)";
@@ -29,25 +33,17 @@ function check_code($errors) {
     $code_count = pwg_db_num_rows($check_return);
     if ($code_count == 0) {
       $errors[] = l10n('Invalid Registration Code');
-    }else{
-	$check_used = 'select used,uses from ' . $prefixeTable . "register_codes where code='$register_code' and (expiry>='$now' or expiry IS NULL)";
-	list($used,$uses) = pwg_db_fetch_row( pwg_query($check_used) );
-	if($used >= $uses ) {
-	  if($uses == 0) {
-	        $used++;
-                $update_used = 'update ' . $prefixeTable . "register_codes set used = $used where code='$register_code' and (expiry>='$now' or expiry IS NULL)";
-  	        pwg_query($update_used);
-
-	  }else{
-		$errors[] = l10n('Invalid Registration Code');
-	  }
-	}else{
-          $used++;
-          $update_used = 'update ' . $prefixeTable . "register_codes set used = $used where code='$register_code' and (expiry>='$now' or expiry IS NULL)";
-          pwg_query($update_used);
-	}
+    } else {
+      $check_used = 'select used,uses from ' . $prefixeTable . "register_codes where code='$register_code' and (expiry>='$now' or expiry IS NULL)";
+      list($used, $uses) = pwg_db_fetch_row(pwg_query($check_used));
+      if ($used <= $uses && $uses != 0) {
+        $used++;
+        $_SESSION['user_register_code'] = $register_code;
+      } else {
+        $errors[] = l10n('Invalid Registration Code');
+      }
     }
-  }else{
+  } else {
     $errors[] = l10n('Invalid Registration Code');
   }
   return $errors;
